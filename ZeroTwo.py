e0f7fcd00c463fbb67eb40076b7298ec8c725d90
MdNoor786 <MdNoor786@users.noreply.github.com>
2021-09-15 16:11:37 +0000

    pylint: auto fixes

--- ZeroTwo.py
+++ ZeroTwo.py
@@ -6,9 +6,11 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/STBxD/XForce-Userbot && cd ZeroTwo && python3 -m userbot")
+system(
+    "git clone https://gitHub.com/STBxD/XForce-Userbot && cd ZeroTwo && python3 -m userbot"
+)
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @XForceSupport")
 # Script written for stop heroku suspension
 # use with credit else gay
-#tf someone already removed the credit where i stoled dis. So he's a gay kek
+# tf someone already removed the credit where i stoled dis. So he's a gay kek
