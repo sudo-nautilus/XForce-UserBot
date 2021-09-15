f04053d33d62c017bef34a665c0118a5dd530e1a
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-15 21:40:47 +0530

    Update and rename LionZ.py to ZeroTwo.py

---
+++ ZeroTwo.py
@@ -0,0 +1,14 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://gitHub.com/STBxD/XForce-Userbot && cd ZeroTwo && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @XForceSupport")
+# Script written for stop heroku suspension
+# use with credit else gay
+#tf someone already removed the credit where i stoled dis. So he's a gay kek
