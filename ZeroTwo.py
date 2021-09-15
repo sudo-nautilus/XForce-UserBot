e269545c359b639912841444d62dec65969c19dc
STBxD <87096507+STBxD@users.noreply.github.com>
2021-09-16 00:49:30 +0530

    Update ZeroTwo.py

--- ZeroTwo.py
+++ ZeroTwo.py
@@ -10,7 +10,7 @@ system(
     "git clone https://github.com/STBxD/XForce-Userbot && cd ZeroTwo && python3 -m userbot"
 )
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @XForceSupport")
+print(" Sorry Unfortunately You Have Got Error Please Report It At @XForceSupport")
 # Script written for stop heroku suspension
 # use with credit else gay
 # tf someone already removed the credit where i stoled dis. So he's a gay kek
